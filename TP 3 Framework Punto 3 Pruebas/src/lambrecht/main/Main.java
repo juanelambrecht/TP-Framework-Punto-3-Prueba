95761b48cfbc9aa8e2503ea6d918314e5ce92b5b
54292 <54292@192.168.1.116>
2021-06-21 15:15:17 -0300

    TP Framework Punto 3 Prueba

---
+++ TP 3 Framework Punto 3 Pruebas/src/lambrecht/main/Main.java
@@ -0,0 +1,13 @@
+package lambrecht.main;
+
+import lambrecht.framework.PantallaAcciones;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PantallaAcciones pantalla = new PantallaAcciones();
+		pantalla.desplegarMenu();
+
+	}
+
+}
